e14244c99e4e6e993dfdef483b63a38aaaa7a590
Radek <plocharadoslaw@gmail.com>
2023-04-09 20:59:29 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const SpotifyWebApi = require('spotify-web-api-node');
+const port = 5000;
+
+dotenv.config();
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/auth/login', (req, res) => {
+  const code = req.body.code;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: 'http://localhost:5173',
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  });
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      //   spotifyApi.setAccessToken(data.body.access_token);
+      //   spotifyApi.setRefreshToken(data.body['refresh_token']);
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })        
+    .catch(() => {
+      res.sendStatus(400);
+    });
+});
+
+app.post('/auth/refresh', (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  console.log('asd');
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: 'http://localhost:5173',
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    refreshToken,
+  });
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch((err) => {
+      res.sendStatus(400);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
