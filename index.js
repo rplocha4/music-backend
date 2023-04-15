7f9e14d71830d9f3a6696d0aeed3c9305850675f
Radek <plocharadoslaw@gmail.com>
2023-04-15 16:12:47 +0200

    added login and register endpoints

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const SpotifyWebApi = require('spotify-web-api-node');
+const connectDB = require('./config/db.js');
+const bcrypt = require('bcrypt');
+const User = require('./db/userModel');
+const jwt = require('jsonwebtoken');
+const auth = require('./auth');
+
 const port = 5000;
 
 dotenv.config();
@@ -11,10 +17,95 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+connectDB();
+
+app.get('/auth', auth, (request, response) => {
+  response.json({ message: 'authorized' });
+});
+
+app.post('/register', (request, response) => {
+  console.log(request.body.password);
+  bcrypt
+    .hash(request.body.password, 10)
+    .then((hashedPassword) => {
+      const user = new User({
+        email: request.body.email,
+        password: hashedPassword,
+      });
+
+      user
+        .save()
+        .then((result) => {
+          response.status(201).send({
+            message: 'User Created Successfully',
+            result,
+          });
+        })
+        .catch((error) => {
+          response.status(500).send({
+            message: 'Error creating user',
+            error,
+          });
+        });
+    })
+    .catch((e) => {
+      response.status(500).send({
+        message: 'Password was not hashed successfully',
+        e,
+      });
+    });
+});
+
+app.post('/login', (request, response) => {
+  User.findOne({ email: request.body.email })
+
+    // if email exists
+    .then((user) => {
+      bcrypt
+        .compare(request.body.password, user.password)
+
+        .then((passwordCheck) => {
+          if (!passwordCheck) {
+            return response.status(400).send({
+              message: 'Passwords does not match',
+              error,
+            });
+          }
+
+          const token = jwt.sign(
+            {
+              userId: user._id,
+              userEmail: user.email,
+            },
+            'RANDOM-TOKEN',
+            { expiresIn: '24h' }
+          );
+
+          response.status(200).send({
+            message: 'Login Successful',
+            email: user.email,
+            token,
+          });
+        })
+        .catch((error) => {
+          response.status(400).send({
+            message: 'Passwords does not match',
+            error,
+          });
+        });
+    })
+    .catch((e) => {
+      response.status(404).send({
+        message: 'Email not found',
+        e,
+      });
+    });
+});
+
 app.post('/auth/login', (req, res) => {
   const code = req.body.code;
   const spotifyApi = new SpotifyWebApi({
-    redirectUri: 'https://music-app-oq29.onrender.com',
+    redirectUri: 'http://localhost:5173',
     clientId: process.env.SPOTIFY_CLIENT_ID,
     clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
   });
@@ -22,14 +113,12 @@ app.post('/auth/login', (req, res) => {
   spotifyApi
     .authorizationCodeGrant(code)
     .then((data) => {
-      //   spotifyApi.setAccessToken(data.body.access_token);
-      //   spotifyApi.setRefreshToken(data.body['refresh_token']);
       res.json({
         accessToken: data.body.access_token,
         refreshToken: data.body.refresh_token,
         expiresIn: data.body.expires_in,
       });
-    })        
+    })
     .catch(() => {
       res.sendStatus(400);
     });
@@ -39,7 +128,7 @@ app.post('/auth/refresh', (req, res) => {
   const refreshToken = req.body.refreshToken;
   console.log('asd');
   const spotifyApi = new SpotifyWebApi({
-    redirectUri: 'https://music-app-oq29.onrender.com',
+    redirectUri: 'http://localhost:5173',
     clientId: process.env.SPOTIFY_CLIENT_ID,
     clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
     refreshToken,
