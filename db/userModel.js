843a26a231c73ef2b66f9c346fcbea5ce0911786
Radek <plocharadoslaw@gmail.com>
2023-04-16 13:33:22 +0200

    added fields to schema like following artists etc

--- db/userModel.js
+++ db/userModel.js
@@ -5,12 +5,35 @@ const UserSchema = new mongoose.Schema({
     required: [true, 'Please provide an Email!'],
     unique: [true, 'Email Exist'],
   },
-
+  username: {
+    type: String,
+    required: [true, 'Please provide a username!'],
+    unique: true,
+  },
   password: {
     type: String,
     required: [true, 'Please provide a password!'],
     unique: false,
   },
+  likedPlaylists: {
+    type: Array,
+    required: false,
+    unique: false,
+  },
+  followingArtists: {
+    type: Array,
+    required: false,
+    unique: false,
+  },
+  followingUsers: {
+    type: Array,
+    required: false,
+    unique: false,
+  },
+  likedSongs: {
+    type: Array,
+    required: false,
+    unique: false,
+  },
 });
 module.exports = mongoose.model.Users || mongoose.model('Users', UserSchema);
-
