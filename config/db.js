3320f2412d1f43b4850b73e7a7fa13c609089753
Radek <plocharadoslaw@gmail.com>
2023-06-16 10:50:35 +0200

    updated mongo connection

--- config/db.js
+++ config/db.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://127.0.0.1:27017/spotify', {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
+    const conn = await mongoose.connect(
+      process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/spotify',
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      }
+    );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
